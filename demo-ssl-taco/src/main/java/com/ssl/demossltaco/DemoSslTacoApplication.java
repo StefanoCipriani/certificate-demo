75b0cb4baeea8b798f11281a85f0eed063b39cc3
StefanoCipriani <stefano.voce@gmail.com>
2021-08-07 15:52:05 +0200

    main classes

---
+++ demo-ssl-taco/src/main/java/com/ssl/demossltaco/DemoSslTacoApplication.java
@@ -0,0 +1,13 @@
+package com.ssl.demossltaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSslTacoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSslTacoApplication.class, args);
+	}
+
+}
---
+++ demo-ssl-taco/src/test/java/com/ssl/demossltaco/DemoSslTacoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssl.demossltaco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSslTacoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
